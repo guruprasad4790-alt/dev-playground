ee45d6fee56a01e3303afa6047036ab2348628b7
unknown <Guru Prasad M>
2026-05-21 23:52:58 +0530

    Practice Python input handling and f-strings

---
+++ python/basics/intro.py
@@ -0,0 +1,9 @@
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+quote = input("Enter quote: ")
+print(f"""
+Hi, My name is {name}
+I am {age} years old
+My favorite quote is: {quote}
+Quote Length is {len(quote)}
+""")

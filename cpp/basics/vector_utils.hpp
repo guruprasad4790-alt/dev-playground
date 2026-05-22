e43fa78ea7912c481b23fc26e2d4736a5dd6daf4
unknown <Guru Prasad M>
2026-05-22 17:53:13 +0530

    Made generic with templates
    - not all functions will work for complex types
    - get stats will work for generic types

--- cpp/basics/vector_utils.hpp
+++ cpp/basics/vector_utils.hpp
@@ -5,16 +5,20 @@
 #include <unordered_set>
 #include <algorithm>
 #include <string>
+#include <limits>
 
 using namespace std;
 
-void printVector(const vector<int>& v, const string& msg = "")
+template <typename T>
+void printVector(const vector<T>& v, const string& msg = "")
 {
     if (!msg.empty()) cout << msg << endl;
-    for (int x : v) { cout << x << " "; }
-    cout << endl;}
+    for (const T& x : v) { cout << x << " "; }
+    cout << endl;
+}
 
-void printReverse(const vector<int>& v)
+template <typename T>
+void printReverse(const vector<T>& v)
 {
     for (auto iter = v.rbegin(); iter != v.rend(); ++iter)
     {
@@ -23,45 +27,54 @@ void printReverse(const vector<int>& v)
     cout << endl;
 }
 
-vector<int> readVector()
+template <typename T>
+vector<T> readVector()
 {
     cout << "Enter the vector elements seperated by spaces: ";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
     string line;
     getline(cin, line);
     stringstream ss(line);
-    int x;
-    vector<int> v;
+
+    T x;
+    vector<T> v;
+    
     while (ss >> x)
-    {
         v.push_back(x);
-    }
+    
     return v;
 }
 
-void getStats(const vector<int>& v)
+// treating this as numeric only for now
+template <typename T>
+void getStats(const vector<T>& v)
 {
-    if (!v.empty())
+    if (v.empty()) return;
+
+    T minNum = v[0];
+    T maxNum = v[0];
+    T sum = T();   // default 0
+
+    for (const T& x : v)
     {
-        int minNum = v[0], maxNum = v[0];
-        int sum = 0;
-        for (int i : v)
-        {
-            minNum = min (minNum, i);
-            maxNum = max (maxNum, i);
-            sum += i;
-        }
-        cout << "Vector stats:\n" 
-             << "Min value= " << minNum << "\n"
-             << "Max value= " << maxNum << "\n"
-             << "Sum= " << sum << endl;
+        minNum = min(minNum, x);
+        maxNum = max(maxNum, x);
+        sum += x;
     }
+
+    cout << "Vector stats:\n"
+         << "Min = " << minNum << "\n"
+         << "Max = " << maxNum << "\n"
+         << "Sum = " << sum << endl;
 }
 
-vector<int> dedupOrdered(const vector<int>& v)
+template <typename T>
+vector<T> dedupOrdered(const vector<T>& v)
 {
-    vector<int> dedup_ordered;
-    unordered_set<int> seen;
-    for (int x : v)
+    vector<T> dedup_ordered;
+    unordered_set<T> seen;
+    for (const T& x : v)
     {
         if(!seen.count(x))
         {
@@ -72,9 +85,10 @@ vector<int> dedupOrdered(const vector<int>& v)
     return dedup_ordered;
 }
 
-vector<int> dedupSorted(const vector<int>& v)
+template <typename T>
+vector<T> dedupSorted(const vector<T>& v)
 {
-    vector<int> dedup_sorted = v;
+    vector<T> dedup_sorted = v;
     sort(dedup_sorted.begin(), dedup_sorted.end());
     dedup_sorted.erase(unique(dedup_sorted.begin(), dedup_sorted.end()), dedup_sorted.end());
     return dedup_sorted;

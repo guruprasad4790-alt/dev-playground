5637c3a214d7795fdecf455c89be457c85795b07
unknown <Guru Prasad M>
2026-05-23 22:29:26 +0530

    Used vector_utils in vector_playground

--- cpp/basics/vector_playground.cpp
+++ cpp/basics/vector_playground.cpp
@@ -1,101 +1,38 @@
 #include <iostream>
-#include <vector>
-#include <sstream>
-#include <algorithm>
-#include <unordered_set>
+#include "vector_utils.hpp"
 
-using namespace std;
-
-void printVector(const vector<int>& v, const string& msg = "")
-{
-    if (!msg.empty()) cout << msg << endl;
-    for (int x : v) cout << x << " ";
-    cout << endl;
-}
-
-void readVector (vector <int>& original)
-{
-    original.clear();
-    cout << "Enter the integer elements of vector: " << endl;
-    string line;
-    getline(cin, line);
-    int x;
-    stringstream ss(line);
-
-    while (ss >> x)
-    {
-        original.push_back(x);
-    }
-}
-
-void printMinMaxSum(const vector<int> & original)
-{    
-    if (!original.empty())
-    {
-        int maxNum = original[0], minNum = original[0], sum = 0;
-        for (int i : original)
-        {
-            maxNum = max(maxNum, i);
-            minNum = min(minNum, i);
-            sum += i;
-        }
-           
-        cout << "Max element is: " << maxNum << ", "
-             << "Min element is: " << minNum << ", "
-             << "Sum of elements is: " << sum << endl;
-    }
-}
 
 int main()
 {
-    vector <int> original;
-    readVector(original);
+    vector <int> original = vecutils::read_vector<int>();
 
-    printVector(original, "The elements of the vector are: ");
-
-    cout << "The elements of the vector in reverse order: " << endl;
-    for (auto iter = original.rbegin(); iter != original.rend(); ++iter)
-    {
-        cout << *iter << " ";
-    }
-    cout << endl;
+    vecutils::print_vector(original, "The elements of the vector are: ");
+    
+    std::cout << "The elements of the vector in reverse order: " << std::endl;
+    
+    vecutils::print_reverse(original);
 
     vector<int> asc = original;
     sort(asc.begin(), asc.end());
-    printVector(asc, "The elements of the ascending ordered vector: ");
+    vecutils::print_vector(asc, "The elements of the ascending ordered vector: ");
 
     vector<int> desc = original;
     sort(desc.begin(), desc.end(), [](int a, int b){
         return a > b ;
     });
-    printVector(desc, "The elements of the descending ordered vector: ");
+    vecutils::print_vector(desc, "The elements of the descending ordered vector: ");
 
-    printMinMaxSum(original);
-
-
+    vecutils::get_stats(original);
+    
     // Remove duplicates can be done by two approaches
     // 1. unsorted array duplicate remove
-    vector<int> dedup_order_preserved;
-    unordered_set<int> seen;
-    for (const auto& i: original)
-    {
-        if(!seen.count(i))
-        {
-            seen.insert(i);
-            dedup_order_preserved.push_back(i);
-        }
-    }
-    printVector(dedup_order_preserved, "Uniq elements without sorting: ");
-
+    
+    vector<int> dedup_order_preserved = vecutils::dedup_ordered(original);
+    vecutils::print_vector(dedup_order_preserved, "Uniq elements without sorting: ");
 
     // 2. sorted array duplicate remove
-    vector<int> dedup_sorted = asc;
-    /*
-    auto iter = unique(dedup_sorted.begin(), dedup_sorted.end());
-    dedup_sorted.erase(iter,dedup_sorted.end());
-    */
-    dedup_sorted.erase (unique (dedup_sorted.begin(), dedup_sorted.end()), dedup_sorted.end());
-    printVector(dedup_sorted, "Uniq elements with sorting: ");
+    vector<int> dedup_sorted = vecutils::dedup_sorted(original);
+    vecutils::print_vector(dedup_order_preserved, "Uniq elements with sorting: ");
 
     return 0;
 
--- cpp/basics/vector_utils.hpp
+++ cpp/basics/vector_utils.hpp
@@ -9,87 +9,141 @@
 
 using namespace std;
 
-template <typename T>
-void printVector(const vector<T>& v, const string& msg = "")
+namespace vecutils
 {
-    if (!msg.empty()) cout << msg << endl;
-    for (const T& x : v) { cout << x << " "; }
-    cout << endl;
-}
-
-template <typename T>
-void printReverse(const vector<T>& v)
-{
-    for (auto iter = v.rbegin(); iter != v.rend(); ++iter)
+    template <typename T>
+    void print_vector(const vector<T>& v, const string& msg = "")
     {
-        cout << *iter << " "; 
-    }
-    cout << endl;
-}
-
-template <typename T>
-vector<T> readVector()
-{
-    cout << "Enter the vector elements seperated by spaces: ";
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    string line;
-    getline(cin, line);
-    stringstream ss(line);
-
-    T x;
-    vector<T> v;
-    
-    while (ss >> x)
-        v.push_back(x);
-    
-    return v;
-}
-
-// treating this as numeric only for now
-template <typename T>
-void getStats(const vector<T>& v)
-{
-    if (v.empty()) return;
-
-    T minNum = v[0];
-    T maxNum = v[0];
-    T sum = T();   // default 0
-
-    for (const T& x : v)
-    {
-        minNum = min(minNum, x);
-        maxNum = max(maxNum, x);
-        sum += x;
+        if (!msg.empty()) cout << msg << endl;
+        for (const T& x : v) { cout << x << " "; }
+        cout << endl;
     }
 
-    cout << "Vector stats:\n"
-         << "Min = " << minNum << "\n"
-         << "Max = " << maxNum << "\n"
-         << "Sum = " << sum << endl;
-}
-
-template <typename T>
-vector<T> dedupOrdered(const vector<T>& v)
-{
-    vector<T> dedup_ordered;
-    unordered_set<T> seen;
-    for (const T& x : v)
+    template<typename T>
+    void print_vector(const vector<T>& v)
     {
-        if(!seen.count(x))
+        for (const auto& x : v) { cout << x << " "; }
+        cout << endl;
+    }
+
+    template<typename T>
+    vector<T> input_vector(int n)
+    {
+        vector<T> v;
+        v.reserve (n);
+        for (int i=0; i< n; i++)
         {
-            seen.insert(x);
-            dedup_ordered.push_back(x);
+            T x;
+            cin >> x;
+            v.push_back(x);
         }
+        return v;
     }
-    return dedup_ordered;
-}
 
-template <typename T>
-vector<T> dedupSorted(const vector<T>& v)
-{
-    vector<T> dedup_sorted = v;
-    sort(dedup_sorted.begin(), dedup_sorted.end());
-    dedup_sorted.erase(unique(dedup_sorted.begin(), dedup_sorted.end()), dedup_sorted.end());
-    return dedup_sorted;
+    template<typename T>
+    T vector_sum(const vector<T>& v)
+    {
+        T sum = T();
+        for (const auto& x : v) { sum+=x; }
+        return sum;
+    }
+
+    template<typename T>
+    T vector_min(const vector<T>& v)
+    {
+        T x;
+        if (v.empty()) return x;
+        T minVal = v[0];
+        for (const auto& x : v) { minVal = min (minVal, x); }
+        return minVal;
+    }
+
+    template<typename T>
+    T vector_max(const vector<T>& v)
+    {
+        T x;
+        if (v.empty()) return T();
+        T maxVal = v[0];
+        for (const auto& x : v) { maxVal = max (maxVal, x); }
+        return maxVal;
+    }
+
+
+    template <typename T>
+    void print_reverse(const vector<T>& v)
+    {
+        for (auto iter = v.rbegin(); iter != v.rend(); ++iter)
+        {
+            cout << *iter << " "; 
+        }
+        cout << endl;
+    }
+
+    template <typename T>
+    vector<T> read_vector()
+    {
+        cout << "Enter the vector elements seperated by spaces: ";
+        //cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+
+        T x;
+        vector<T> v;
+        
+        while (ss >> x)
+            v.push_back(x);
+        
+        return v;
+    }
+
+    // treating this as numeric only for now
+    template <typename T>
+    void get_stats(const vector<T>& v)
+    {
+        if (v.empty()) return;
+
+        T minNum = v[0];
+        T maxNum = v[0];
+        T sum = T();   // default 0
+
+        for (const T& x : v)
+        {
+            minNum = min(minNum, x);
+            maxNum = max(maxNum, x);
+            sum += x;
+        }
+
+        cout << "Vector stats:\n"
+            << "Min = " << minNum << "\n"
+            << "Max = " << maxNum << "\n"
+            << "Sum = " << sum << endl;
+    }
+
+    template <typename T>
+    vector<T> dedup_ordered(const vector<T>& v)
+    {
+        vector<T> dedup_ordered;
+        unordered_set<T> seen;
+        for (const T& x : v)
+        {
+            if(!seen.count(x))
+            {
+                seen.insert(x);
+                dedup_ordered.push_back(x);
+            }
+        }
+        return dedup_ordered;
+    }
+
+    template <typename T>
+    vector<T> dedup_sorted(const vector<T>& v)
+    {
+        vector<T> dedup_sorted = v;
+        sort(dedup_sorted.begin(), dedup_sorted.end());
+        dedup_sorted.erase(unique(dedup_sorted.begin(), dedup_sorted.end()), dedup_sorted.end());
+        return dedup_sorted;
+    }
+
 }
